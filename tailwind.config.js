ce1f615afe5770f550fbef876c7ddcbd6b70dcd2
Vidhan Bhatt <me@vidhan.io>
2021-11-19 16:21:17 -0500

    past-seasons done!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 module.exports = {
   mode: "jit",
   purge: ["./**/*.html"],
-  darkMode: "media", // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     extend: {
       backgroundImage: {
         index:
           "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/index/hero.jpg')",
-        about:
-          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/about/hero.jpg')",
+        "about-us":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/about-us/hero.jpg')",
+        "past-seasons":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/past-seasons/hero.jpg')",
       },
       width: {
         128: "32rem",
       },
       height: {
         128: "32rem",
-      },
-      rotate: {
-        360: "360deg",
       },
     },
   },
