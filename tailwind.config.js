66061f21a5f5fefb6059d4afe5ccd3c2123794bf
Vidhan Bhatt <me@vidhan.io>
2021-11-19 20:20:42 -0500

    switch to main tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
           "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/past-seasons/hero.jpg')",
         "sponsor-us":
           "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/sponsor-us/hero.jpg')",
+        sponsors:
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/sponsors/hero.jpg')",
       },
       width: {
         128: "32rem",
