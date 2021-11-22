a9b74827a83a8a988ec9df705f094e93ea39bad6
Vidhan Bhatt <me@vidhan.io>
2021-11-22 13:26:00 -0500

    Add light mode and fix windows scroll bar (#3)
    * add cname
    
    * add overflow hidden
    
    * Delete CNAME
    
    * lol fix y scroll
    
    * change classname
    
    * add light mode support
    
    * fix light mode consistency
    
    * fix missions

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,24 @@ module.exports = {
     extend: {
       backgroundImage: {
         index:
-          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/index/hero.jpg')",
+          "linear-gradient(to top, rgba(209, 213, 219, 1), rgba(209, 213, 219, 0.5)), url('../assets/images/index/hero.jpg')",
         "about-us":
-          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/about-us/hero.jpg')",
+          "linear-gradient(to top, rgba(209, 213, 219, 1), rgba(209, 213, 219, 0.5)), url('../assets/images/about-us/hero.jpg')",
         "past-seasons":
-          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/past-seasons/hero.jpg')",
+          "linear-gradient(to top, rgba(209, 213, 219, 1), rgba(209, 213, 219, 0.5)), url('../assets/images/past-seasons/hero.jpg')",
         "sponsor-us":
-          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/sponsor-us/hero.jpg')",
+          "linear-gradient(to top, rgba(209, 213, 219, 1), rgba(209, 213, 219, 0.5)), url('../assets/images/sponsor-us/hero.jpg')",
         sponsors:
+          "linear-gradient(to top, rgba(209, 213, 219, 1), rgba(209, 213, 219, 0.5)), url('../assets/images/sponsors/hero.jpg')",
+        "dark-index":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/index/hero.jpg')",
+        "dark-about-us":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/about-us/hero.jpg')",
+        "dark-past-seasons":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/past-seasons/hero.jpg')",
+        "dark-sponsor-us":
+          "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/sponsor-us/hero.jpg')",
+        "dark-sponsors":
           "linear-gradient(to top, rgba(17, 24, 39, 1), rgba(17, 24, 39, 0.5)), url('../assets/images/sponsors/hero.jpg')",
       },
       width: {
